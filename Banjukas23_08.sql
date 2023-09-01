142840782364b33bd338dbe149d71c56d1a3f4d6
LARYSSA MACHADO HUGEM <laryssa.mhugem@senacsp.edu.br>
2023-08-31 22:09:33 -0300

    ATV procedure add Cliente/Correntista 31_08

--- Banjukas23_08.sql
+++ Banjukas23_08.sql
@@ -17,3 +17,23 @@ select * from conta
 where idCliente=@idCliente
 
 insert into Cliente values ('Laryssa', '83763521453', '66597632', '12/12/2003 12:45', 'F', '13965487562', '39678542', 'Rua Luiz de Camões', '25','Casa', 'Santos', 'São Paulo', 2300.00, 'lary@gmail.com', '123456');
+
+create procedure pi_Correntista
+@nomeCliente varchar(200),
+@cpf char(11),
+@rg char(8),
+@dataNascimento datetime,
+@genero char(1),
+@celular char(11),
+@cep char(8),
+@logradouro varchar(50),
+@numeroEndereco char(5),
+@complemento varchar(100),
+@cidade varchar(50),
+@estado varchar(50),
+@renda decimal(10,2),
+@email varchar(50),
+@senha varchar(6)
+as
+insert into Cliente
+values (@nomeCliente, @cpf, @rg, @dataNascimento, @genero, @celular, @cep, @logradouro, @numeroEndereco, @complemento, @cidade, @estado, @renda, @email, @senha)
